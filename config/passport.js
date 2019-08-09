f84dcdce91afa2a7c2825bf3f1201776a169351d
Email Update <haojinzhao@yahoo.com>
2019-08-09 12:59:35 -0500

    add user signup/signin/logout functions

---
+++ config/passport.js
@@ -0,0 +1,120 @@
+var bcrypt = require("bcryptjs");
+
+module.exports = function (passport, user){
+
+    
+    var User = user;
+    var LocalStrategy = require("passport-local").Strategy;
+    //Passport will maintain persistent login sessions. In order for persistent sessions to work, the authenticated user must be serialized to the session, and deserialized when subsequent requests are made.
+    //serialize user
+    passport.serializeUser(function(user,done){
+        done(null,user.id);
+    });
+
+    //deserialize user
+    passport.deserializeUser(function(id,done){
+        User.findOne({where:{id:id}}).then(function(user){
+            if(user){
+                done(null,user.get());
+            }
+        })
+
+    });
+
+    //local sign up
+    passport.use("local-signup",new LocalStrategy({
+        usernameField: "email",
+        passwordField: "password",
+        passReqToCallback:true
+        //allow pass back the entire request to the callback
+
+    }, function (req,email,password,done){
+         var generateHash = function(password){
+
+             return bcrypt.hashSync(password,bcrypt.genSaltSync(8),null);
+         };
+
+         console.log("req",req);
+         console.log("email",email);
+         console.log("pass",password);
+         console.log("user",User);
+         User.findOne({
+             where: {
+                 email: email
+             }
+         }).then(function(user){
+               console.log("sign up user",user);
+               if ( user){
+                   console.log("email taken done");
+                   return done(null,false, {message : "That email is already taken"});
+                   //return res.send({ success : false, message : 'That email is already taken' }); jz added
+               } else {
+                   var userPassword = generateHash(password);
+                   var data = {
+                       email: email,
+                       password: userPassword,
+                       firstName: req.body.firstName,
+                       lastName: req.body.lastName,
+                       userName: req.body.userName
+                       };
+                   
+                   User.create(data).then(function(newUser,created){
+                       if (!newUser){
+                           return done (null,false);  
+                        }
+                       if (newUser) {
+                           return done(null, newUser)
+                       }   
+                   });
+               }
+         });
+    
+    }));
+
+
+    //LOCAL SIGNIN
+    passport.use('local-signin', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true //  pass back the entire request to the callback
+    }, function (req, email, password, done) {
+        var User = user;
+
+        console.log("signin  email",email);
+        console.log("password",password);
+        var isValidPassword = function (userpass, password) {
+            return bcrypt.compareSync(password, userpass);
+        }
+
+        User.findOne({
+            where: {
+                email: email
+            }
+        }).then(function (user) {
+            console.log("after findOne, user",user);
+            if (!user) {
+                return done(null, false, { message: 'Email does not exist' });
+            }
+
+            if (!isValidPassword(user.password, password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+
+            var userinfo = user.get();
+            console.log("login return userinfo",userinfo);
+            // return res.status(200)
+            //           .json( {id:uerinfo.id,
+            //                   userName:userinfo.userName,
+            //                   firstName:userinfo.firstName,
+            //                   lastName: userinfo.lastName,
+            //                   accessLevel:userinfo.accessLevel,
+            //                   createdAt: userinfo.createdAt,
+            //                   updatedAt: userinfo.updatedAt});
+            return done(null, userinfo);
+        }).catch(function (err) {
+            console.log("Error:", err);
+            return done(null, false, { message: 'Something went wrong with your Signin' });
+        });
+    }));
+}
--- server.js
+++ server.js
@@ -12,24 +12,46 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Requiring our models for syncing
 var db = require("./models");
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// // Sets up the Express app to handle data parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // Static directory
 app.use(express.static("public"));
 
+//For passport
+app.use(session({ secret: 'my secret', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Routes
 // =============================================================
 
-// require("./controllers/usersController.js")(app);
+var authRoute = require('./controllers/auth.js')(app, passport);
+
+//Load passport strategies
+require('./config/passport.js')(passport, db.User);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
