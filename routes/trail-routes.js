f2b2ad856545c6581df7caa6af9deb32f63531b0
Paris Cornett <pariscornett@gmail.com>
2019-08-09 23:21:58 -0500

    added routes for all existing endpoints

--- routes/trail-routes.js
+++ routes/trail-routes.js
@@ -6,11 +6,35 @@ var app = express();
 //var trails = require("../public/assets/js/trails.js");
 
 
-//route to display the index page at /signup OR root /
-app.get("/signup" || "/", function(req,res) {
+//route to display the index page at /signup OR root / OR after logging out
+app.get("/signup" || "/" || "/logout", function(req,res) {
     res.render("index");
 });
 
+//route to lead to dashboard after successful login
+app.get("/login", function(req,res) {
+    res.render("dashboard-info");
+});
+
+//route to display the "Retrieve All Trails in My City" page
+app.get("/trails/:city", function(req,res) {
+    res.render("dashboard-search");
+});
+
+//route to display the "Add a new Trail" page
+app.get("/trails/add", function(req,res) {
+    res.render("add-trail");
+});
+
+//route to display the "Update Trail" page
+app.get("/trails/update", function(req,res) {
+    res.render("update-trail");
+});
+
+//route to display the "Delete Trail" warning
+app.get("/trails/delete", function(req,res) {
+    res.render("delete-trail");
+});
 
 
 
