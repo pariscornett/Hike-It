81ad0d57fc907cc2d35e1b6766cfb5847b42fa3d
Paris Cornett <pariscornett@gmail.com>
2019-08-11 22:15:01 -0500

    got sign up form to return a newUser object

--- public/assets/js/trail-data.js
+++ public/assets/js/trail-data.js
@@ -1,28 +1,39 @@
-$(function() {
-    $(".form-group").on("submit", function() {
-        //pulls info from "Sign Up" form
-        var userName = $("#userName").val().trim();
-        var email = $("#email").val().trim();
-        var password = $("#InputPassword").val().trim();
-        var confirmPassword = $("#confirmPassword").val().trim();
-        var firstName = $("#inputFirstName").val().trim();
-        var lastName = $("#inputLastName").val().trim();
+$("#submit-btn").on("click", function(event) {
+    // event.preventDefault();
+   // pulls info from "Sign Up" form
+    var userName = $("#userName").val() //works
+    var email = $("#emailInput").val(); //undefined
+    var password = $("#Password").val(); //undefined
+    var confirmPassword = $("#confirmPassword").val(); //works
+    var firstName = $("#FirstName").val(); //undefined
+    var lastName = $("#LastName").val(); //undefined
 
-        //check to make sure password and confirmPassword are identical
-        if (password !== confirmPassword) {
-            alert("Check to make sure your passwords match!");
-        } else {
-            //stores above info in an object to send to backend
-            var newUser = {
-                userName : userName,
-                email : email,
-                password: password,
-                firstName: firstName,
-                lastName: lastName
-            };
-            console.log(newUser);
-        }
+   
+    // console.log(userName);
+    // console.log(email);
+    // console.log(password);
+    // console.log(confirmPassword);
+    // console.log(firstName);
+    // console.log(lastName);
+    
 
-       
-    })
-})
+    //check to make sure password and confirmPassword are identical
+    if (password !== confirmPassword) {
+        alert("Check to make sure your passwords match!");
+    } else {
+        //stores above info in an object to send to backend
+        var newUser = {
+            userName : userName,
+            email : email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        };
+        console.log(newUser);
+    }
+
+    
+});
+
+
+
--- server.js
+++ server.js
@@ -16,6 +16,10 @@ var passport = require('passport');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -31,16 +35,17 @@ app.use(session({ secret: 'my secret', resave: true, saveUninitialized: true }))
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Sets up Express-Handlebars **please leave in here
+//Sets up Express-Handlebars **please leave in here, we need it I promise**
 var exphbs = require("express-handlebars");
 app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
+
+// Routes
+// =============================================================
 var routes = require("./routes/trail-routes.js");
 require("./controllers/trail_controller.js")(app);
 app.use(routes);
 
-// Routes
-// =============================================================
 
 // require("./controllers/usersController.js")(app);
 var authRoute = require('./controllers/auth.js')(app, passport);
