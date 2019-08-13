2a3f5ba18031a384af9bccc6019dca42e6688d4a
unknown <chris.aaron.williams@gmail.com>
2019-08-13 12:24:28 -0500

    changed TN to Tennessee in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO Trail (trailName, trailAddress, trailCity, trailState, trailLength, trailDifficulty) VALUES ('Radnor Lake State Park', '1160 Otter Creek Rd', 'Nashville', 'TN',5,2);
-INSERT INTO Trail (trailName, trailAddress, trailCity, trailState, trailLength, trailDifficulty) VALUES ('Cummins Falls State Park', '390 Cummins Falls Ln', 'Cookeville', 'TN',4,4);
+INSERT INTO Trail (trailName, trailAddress, trailCity, trailState, trailLength, trailDifficulty) VALUES ('Radnor Lake State Park', '1160 Otter Creek Rd', 'Nashville', 'Tennessee',5,2);
+INSERT INTO Trail (trailName, trailAddress, trailCity, trailState, trailLength, trailDifficulty) VALUES ('Cummins Falls State Park', '390 Cummins Falls Ln', 'Cookeville', 'Tennessee',4,4);
 
-INSERT INTO Forum (personalName, email, itemlName, itemDescription, itemCity, itemState, itemPrice) VALUES ('Chris', 'chris@email.com', 'Skateboard', 'lots of ware, about to fall apart', 'Hendersonville', 'TN', 20);
-INSERT INTO Forum (personalName, email, itemlName, itemDescription, itemCity, itemState, itemPrice) VALUES ('Helen', 'helen@email.com', 'Mountain Bike', 'kept in great shape', 'Nashville', 'TN', 400);
+INSERT INTO Forum (personalName, email, itemlName, itemDescription, itemCity, itemState, itemPrice) VALUES ('Chris', 'chris@email.com', 'Skateboard', 'lots of ware, about to fall apart', 'Hendersonville', 'Tennessee', 20);
+INSERT INTO Forum (personalName, email, itemlName, itemDescription, itemCity, itemState, itemPrice) VALUES ('Helen', 'helen@email.com', 'Mountain Bike', 'kept in great shape', 'Nashville', 'Tennessee', 400);
